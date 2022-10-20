113e8a589bd144ab6c543a1505050bbaeaf4086a
facundo <facundo.olano@entropy1729.com>
2022-10-20 20:38:28 -0300

    (failed) attempt at program verification

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,13 @@
 use anyhow::Result;
-use std::path::PathBuf;
 use clap::Parser;
+use std::path::PathBuf;
 
-use snarkvm::{package::Package, prelude::{Testnet3, Identifier}, circuit::AleoV0, prelude::Value};
+use snarkvm::{
+    circuit::AleoV0,
+    package::Package,
+    prelude::Value,
+    prelude::{Identifier, Testnet3},
+};
 
 #[derive(Debug, Parser)]
 pub struct Cli {
@@ -16,7 +21,7 @@ pub struct Cli {
     inputs: Vec<Value<Testnet3>>,
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     let package: Package<Testnet3> = Package::open(cli.path.as_path()).unwrap();
@@ -25,13 +30,22 @@ fn main() -> Result<()>{
     // is this necessary?
     let rng = &mut rand::thread_rng();
 
-    let (_response, _transition) = package.run::<AleoV0, _>(
+    let (response, execution) = package.run::<AleoV0, _>(
         None,
         package.manifest_file().development_private_key(),
         cli.function,
         &cli.inputs,
         rng,
     )?;
+
+    println!("outputs {:?}", response.outputs());
+
+    // verify the execution
+    let process = package.get_process()?;
+    process.verify_execution(&execution)?;
+
+    // TODO once we get the above working, we need to do the same without assuming local files
+    // e.g. the way a node would have to run it if all the necessary context was received in a request
 
     Ok(())
 }
