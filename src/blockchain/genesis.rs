7cbaea3d43589804c34e1b7dce9a1b13025ce09a
Facundo Olano <facundo.olano@entropy1729.com>
2022-12-26 10:58:43 -0300

    Introduce a Validator struct (#90)
    * move genesis state out of lib
    
    * use tendermint key and address structs instead of raw strings and bytes
    
    * add some aliases
    
    * stub validator struct
    
    * store validators instead of a map in the validator set
    
    * refactor validator from str
    
    * load voting power in genesis
    
    * fix tests
    
    * use bytes instead of tendermint account id struct
    
    * some doc comments
    
    * fix tests
    
    * move validator to lib to prevent clippy wanrs
    
    * improve readability
    
    * more readability
    
    * typo
    
    * clippy
    
    * more clippy

--- src/blockchain/genesis.rs
+++ src/blockchain/genesis.rs
@@ -1,11 +1,11 @@
 /// Binary that walks a list of tendermint node directories (like the default ~/.tendermint or a testnet generated node dir),
 /// assuming they also contain an aleo account credentials file, and updates their genesis files to include the genesis state
 /// expected by our abci app.
-use std::{collections::HashMap, path::PathBuf, str::FromStr};
+use std::{collections::HashMap, path::PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
-use lib::{vm, GenesisState};
+use lib::{validator, vm};
 
 /// Takes a list of node directories and updates the genesis files on each of them
 /// to include records to assign default credits to each validator and a mapping
@@ -28,35 +28,6 @@ pub struct Cli {
 fn main() -> Result<()> {
     let cli: Cli = Cli::parse();
 
-    // for each node in the testnet, map its tendermint pubkey to its aleo account address
-    // and generate records for initial validator credits
-    let mut address_map = HashMap::new();
-    let mut genesis_records = Vec::new();
-    for node_dir in cli.node_dirs.clone() {
-        println!("processing {}", node_dir.to_string_lossy());
-
-        let aleo_account_path = node_dir.join("account.json");
-        let aleo_account: serde_json::Value =
-            serde_json::from_str(&std::fs::read_to_string(aleo_account_path)?)?;
-        let aleo_address = vm::Address::from_str(aleo_account["address"].as_str().unwrap())?;
-
-        let tmint_account_path = node_dir.join("config/priv_validator_key.json");
-        let tmint_account: serde_json::Value =
-            serde_json::from_str(&std::fs::read_to_string(tmint_account_path)?)?;
-        let tmint_pubkey = tmint_account["pub_key"]["value"]
-            .as_str()
-            .expect("couldn't extract pubkey from json");
-
-        address_map.insert(tmint_pubkey.to_string(), aleo_address);
-
-        println!("Generating record for {aleo_address}");
-        // NOTE: using a hardcoded seed, not for production!
-        let seed = 123;
-        let record = vm::mint_record("credits.aleo", "credits", &aleo_address, cli.amount, seed)?;
-        genesis_records.push(record);
-    }
-
-    // update the genesis JSON with the calculated app state
     let genesis_path = cli
         .node_dirs
         .first()
@@ -64,9 +35,57 @@ fn main() -> Result<()> {
         .join("config/genesis.json");
     let mut genesis: serde_json::Value =
         serde_json::from_str(&std::fs::read_to_string(genesis_path)?)?;
-    let genesis_state = GenesisState {
+    let voting_powers: HashMap<String, u64> = genesis["validators"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|validator| {
+            (
+                validator["pub_key"]["value"].as_str().unwrap().to_string(),
+                validator["power"].as_str().unwrap().parse().unwrap(),
+            )
+        })
+        .collect();
+
+    // for each node in the testnet, map its tendermint pubkey to its aleo account address
+    // and generate records for initial validator credits
+    let mut validators = Vec::new();
+    let mut genesis_records = Vec::new();
+    for node_dir in cli.node_dirs.clone() {
+        println!("processing {}", node_dir.to_string_lossy());
+
+        let aleo_account_path = node_dir.join("account.json");
+        let aleo_account: serde_json::Value =
+            serde_json::from_str(&std::fs::read_to_string(aleo_account_path)?)?;
+        let aleo_address = aleo_account["address"].as_str().unwrap();
+
+        let tmint_account_path = node_dir.join("config/priv_validator_key.json");
+        let tmint_account: serde_json::Value =
+            serde_json::from_str(&std::fs::read_to_string(tmint_account_path)?)?;
+        let tmint_pubkey = tmint_account["pub_key"]["value"]
+            .as_str()
+            .expect("couldn't extract pubkey from json");
+        let mut validator = validator::Validator::from_str(tmint_pubkey, aleo_address)?;
+        validator.add_voting_power(*voting_powers.get(tmint_pubkey).unwrap() as i64)?;
+
+        println!("Generating record for {aleo_address}");
+        // NOTE: using a hardcoded seed, not for production!
+        let seed = 123;
+        let record = vm::mint_record(
+            "credits.aleo",
+            "credits",
+            &validator.aleo_address,
+            cli.amount,
+            seed,
+        )?;
+        genesis_records.push(record);
+        validators.push(validator);
+    }
+
+    // update the genesis JSON with the calculated app state
+    let genesis_state = validator::GenesisState {
         records: genesis_records,
-        validators: address_map,
+        validators,
     };
     genesis.as_object_mut().unwrap().insert(
         "app_state".to_string(),
