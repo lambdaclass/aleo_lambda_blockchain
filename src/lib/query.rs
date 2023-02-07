e94ce8c92e1b2775c54445ec5e2f64bb9a86a792
Nacho <amigoigna@gmail.com>
2023-02-07 16:38:40 -0300

    wiring each part

--- src/lib/query.rs
+++ src/lib/query.rs
@@ -1,4 +1,4 @@
-use crate::vm::ProgramID;
+use crate::vm::{self, ProgramID};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -7,8 +7,10 @@ pub enum AbciQuery {
     GetRecords,
     /// Returns all spent records's serial numbers
     GetSpentSerialNumbers,
-    /// Returns the program struct given it's id
+    /// Returns the program struct given its id
     GetProgram { program_id: ProgramID },
+    /// Returns a valid merkle path for a record
+    GetMerklePath { ciphertext: vm::Field },
 }
 
 impl From<AbciQuery> for Vec<u8> {
